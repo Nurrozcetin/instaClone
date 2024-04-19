8c488c2baf797c16664a3cacddffc533a08c67d0
Fatmanur Özçetin <93328444+Nurrozcetin@users.noreply.github.com>
2024-04-19 13:09:00 +0300

    create env and ui add firebase

---
+++ app/src/main/java/com/example/instagram/UploadActivity.java
@@ -0,0 +1,25 @@
+package com.example.instagram;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class UploadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload);
+    }
+    public void upload(View view ){
+
+    }
+    public void imageSelect(View view){
+
+    }
+}
