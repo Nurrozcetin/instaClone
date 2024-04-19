680fdd67cd5024c863be9289854a3db8b2fb489e
Fatmanur Özçetin <93328444+Nurrozcetin@users.noreply.github.com>
2024-04-19 16:17:00 +0300

    added menu and image permission

--- app/src/main/java/com/example/instagram/FeedActivity.java
+++ app/src/main/java/com/example/instagram/FeedActivity.java
@@ -1,24 +1,61 @@
 package com.example.instagram;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.instagram.databinding.ActivityFeedBinding;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class FeedActivity extends AppCompatActivity {
+    private FirebaseAuth auth;
+    private ActivityFeedBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_feed);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        binding = ActivityFeedBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        auth = FirebaseAuth.getInstance();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.option_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.add_post){
+            //upload activity
+            Intent intent = new Intent(FeedActivity.this, UploadActivity.class);
+            startActivity(intent);
+        }else if(item.getItemId() == R.id.sign_out){
+            //sign out
+
+            auth.signOut();
+
+            Intent intentToMain = new Intent(FeedActivity.this, MainActivity.class);
+            startActivity(intentToMain);
+            finish();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/example/instagram/UploadActivity.java
+++ app/src/main/java/com/example/instagram/UploadActivity.java
@@ -1,25 +1,112 @@
 package com.example.instagram;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.ImageDecoder;
+import android.media.Image;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.instagram.databinding.ActivityUploadBinding;
+import com.google.android.material.snackbar.Snackbar;
+
 public class UploadActivity extends AppCompatActivity {
 
+    private ActivityUploadBinding bindig;
+    Uri imageData;
+    Bitmap selectedImage;
+    ActivityResultLauncher<Intent> activityResultLauncher;
+    ActivityResultLauncher<String> permissionLauncher;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_upload);
+        bindig = ActivityUploadBinding.inflate(getLayoutInflater());
+        View view = bindig.getRoot();
+        setContentView(view);
+
+        registerLauncher();
     }
+
     public void upload(View view ){
 
     }
     public void imageSelect(View view){
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)){
+                Snackbar.make(view, "Permission needed for gallery",Snackbar.LENGTH_INDEFINITE).setAction("Give permission", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        //ask permission
+                        permissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE);
+                    }
+                }).show();
+            }else{
+                //ask permission
+                permissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE);
+            }
+        }else {
+            Intent intentToGallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+            activityResultLauncher.launch(intentToGallery);
+        }
+    }
 
+    private void registerLauncher(){
+        activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult result) {
+                if(result.getResultCode() == RESULT_OK){
+                    Intent intentFromResult = result.getData();
+                    if(intentFromResult != null) {
+                        imageData = intentFromResult.getData();
+                        bindig.imgSelect.setImageURI(imageData);
+
+                        /*change to bitmap
+                        try {
+                            if(Build.VERSION.SDK_INT >= 28){
+                                ImageDecoder.Source source = ImageDecoder.createSource(UploadActivity.this.getContentResolver(), imageData);
+                                selectedImage = ImageDecoder.decodeBitmap(source);
+                                bindig.imgSelect.setImageBitmap(selectedImage);
+                            }else{
+                                selectedImage = MediaStore.Images.Media.getBitmap(UploadActivity.this.getContentResolver(), imageData);
+                                bindig.imgSelect.setImageBitmap(selectedImage);
+                            }
+                        }catch (Exception error){
+                            error.printStackTrace();
+                        }*/
+
+                    }
+                }
+            }
+        });
+
+        permissionLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(), new ActivityResultCallback<Boolean>() {
+            @Override
+            public void onActivityResult(Boolean result) {
+                if(result){
+                    Intent intentToGallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+                    activityResultLauncher.launch(intentToGallery);
+                }else{
+                    Toast.makeText(UploadActivity.this, "Permission needed", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
